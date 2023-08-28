75dc9ec783c172bf6d40eaa3cf12f170d63e499f
MapleNe <mineimc@outlook.com>
2023-08-29 05:20:24 +0800

    初始化评论详情

---
+++ pages/common/article/comment/comment.vue
@@ -0,0 +1,19 @@
+<template>
+	<z-paging ref="paging">
+	
+	</z-paging>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
